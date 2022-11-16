f736cbc6d49ab97b999084ff92c8633b6e1db341
vaishnavi <vkawale2004@gmail.com>
2022-11-16 21:55:07 +0530

    update

---
+++ app.js
@@ -0,0 +1,19 @@
+var btnTranslate = document.querySelector("#btn-translate");
+
+btnTranslate.addEventListener("click", function clickEvenHandler(){
+    console.log("clicked!")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
