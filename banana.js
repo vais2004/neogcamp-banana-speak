21fac938729388143761065c7601976bd4dfeab8
vaishnavi <vkawale2004@gmail.com>
2022-11-17 17:34:42 +0530

    update

---
+++ banana.js
@@ -0,0 +1,19 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput= document.querySelector("#txt-input");
+var outputDiv= document.querySelector("#output");
+
+
+function getTranslationURL(text){
+
+}
+
+
+
+function clickHandler() {
+    outputDiv.innerText="ajajajajajajaj" + txtInput.value;
+};
+
+btnTranslate.addEventListener("click", clickHandler)
+
+
+
