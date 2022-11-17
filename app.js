be02446176e77d24b07057f5c8c388568c236788
vaishnavi <vkawale2004@gmail.com>
2022-11-17 15:19:10 +0530

    update

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput= document.querySelector("#txt-input");
+var outputDiv= document.querySelector("#output");
 
-console.log(txtInput)
+
 
 function clickHandler() {
-    console.log("clicked!")
-    console.log("input", txtInput.value);
+    outputDiv.innerText="ajajajajajajaj" + txtInput.value;
 };
 
 btnTranslate.addEventListener("click", clickHandler)
+
 
 
